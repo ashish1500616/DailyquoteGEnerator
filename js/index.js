b40965f310c5fb30147e40f33fa1e03b8eff9198
Ashish verma <koolashish2011@gmail.com>
2016-12-31 01:25:20 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	$("#new").click(function() {
+		// var cb = Math.round(new Date().getTime() / 1000);
+		$.ajaxSetup({
+			cache: false
+		});
+		$.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
+			console.log(a);
+			$("#quote").html("\""+a[0].content + "<p>&mdash; " + a[0].title +"</br>\""+ "</p>");
+		});
+	});
+});
+//what is mdash?
+//what is cross.?
+
+//facebook plugin invoke
